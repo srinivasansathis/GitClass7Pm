867b8a3a82c984c0b5dd41b3e7113bf570dd1e0c
srinivasansathis <srinivasanksthala@gmail.com>
2024-12-31 17:27:29 +0530

    First time push

---
+++ src/test/java/com/git/Sample.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class Sample {
+	
+	public void argentina() {
+		System.out.println("Lionel Messi");
+	}
+	public void portugal() {
+		System.out.println("Cristiano Ronaldo");
+	}
+	public void brazil() {
+		System.out.println("Neymar");
+	}
+	
+	
+
+}
